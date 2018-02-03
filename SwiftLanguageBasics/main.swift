90c4d4ee277f1f2284e595227f09b811bbd06970
Max Taylor <mjttk5@mail.missouri.edu>
2018-02-02 22:45:43 -0600

    First commit

---
+++ SwiftLanguageBasics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLanguageBasics
+//
+//  Created by Max Taylor on 2/2/18.
+//  Copyright © 2018 Max Taylor. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
